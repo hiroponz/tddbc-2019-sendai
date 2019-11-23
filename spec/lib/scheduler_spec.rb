f09281222b8092b84a5c99dc4af4bcb335860d72
Hiroyuki Sato <h-sato@creationline.com>
2019-11-23 13:42:45 +0900

    テストケースを追加

--- spec/lib/scheduler_spec.rb
+++ spec/lib/scheduler_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe Scheduler do
-  describe '0時 0分 0秒 をあたえて、スケジューラー設定オブジェクトを作成する' do
-    let(:scheduler){ Scheduler.new(hour: 0, min: 0, sec: 0) }
+  describe 'スケジューラー設定オブジェクトを作成する' do
+    context '0時 0分 0秒 を与える' do
+      let(:scheduler){ Scheduler.new(hour: 0, min: 0, sec: 0) }
 
-    it do
-      expect{ scheduler }.not_to raise_error
+      it '設定オブジェクトが正常に作成される' do
+        expect{ scheduler }.not_to raise_error
+      end
+    end
+
+    context '引数をを与えない' do
+      let(:scheduler){ Scheduler.new() }
+
+      it '設定オブジェクトの作成で例外が発生する' do
+        expect{ scheduler }.to raise_error(ArgumentError)
+      end
     end
   end
 end
